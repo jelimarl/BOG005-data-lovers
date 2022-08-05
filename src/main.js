d16327323857abb712ee94f5ffca729764a89dba
SoreyC <soreycortes@gmail.com>
2022-08-05 17:40:30 -0500

    Estructura y estilos de la segunda página

--- src/main.js
+++ src/main.js
@@ -1,33 +1,73 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
 import { filterSpellsCharm } from './data.js';
 
 
 import data from './data/harrypotter/data.js';
 
-let spellsContainer = document.getElementById("spellsContainer");
-var spells = data.spells;
-console.log(spells);
+const spellsButton = document.getElementById("spellsButton")
+let spells = data.spells;
 
-for (let spell of spells) {
-    console.log(spell);
+function showSpells(){
+    firstPage.style.display = "none";
+    secondPage.style.display = "block";
 
-    const p = document.createElement("p");
-    const article = document.createElement("article");
-    p.innerText = spell.name;
-    article.appendChild(p);
-    spellsContainer.append(article);
+   
+    for (let spell of spells) {
+       // console.log(spell);
+    
+        const p = document.createElement("p");
+        const article = document.createElement("article");
+        p.innerText = spell.name;
+        article.appendChild(p);
+        spellsContainer.append(article);
+    
+    }
 
 }
+
+
+spellsButton.addEventListener("click",showSpells)
+
+
+
+let spellsContainer = document.getElementById("spellsContainer");
+let spellsContainerCharm = document.getElementById("spellsContainerCharm");
+
+//console.log(spells);
+
+const filterButton = document.getElementById("filterButton");
+
+function showSpellsCharm(){
+    var arrSpellsCharm = spells.filter(filterSpellsCharm);
+
+    thirdPage.style.display = "block";
+    firstPage.style.display = "none";
+    secondPage.style.display = "none";
+
+    for (let spell of arrSpellsCharm) {
+        console.log(spell);
+    
+        const p = document.createElement("p");
+        const article = document.createElement("article");
+        p.innerText = spell.name;
+        article.appendChild(p);
+        spellsContainerCharm.append(article);
+    
+    }
+
+}
+
+
 
 //console.log(spells[3]['name']);
 
 
 
-var arrSpellsCharm = spells.filter(filterSpellsCharm);
-console.log(arrSpellsCharm[0]['name']); 
 
-console.log(example, data);
+
+filterButton.addEventListener("click",showSpellsCharm);
+
+//console.log(arrSpellsCharm[0]['name']); 
+
+//console.log(example, data);
